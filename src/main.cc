aad1487fb3700198cd98f6e322b4faa57090b7c6
abundance <abundanceanyanwu@gmail.com>
2024-05-01 22:47:53 +0100

    organized objects in abstract class

--- src/main.cc
+++ src/main.cc
@@ -1,55 +1,46 @@
 #include <GL/freeglut.h>
 // #include <GL/glut.h>
-#include <vector>
 #include <algorithm>
-#include <iostream>
 #include <cmath>
 #include <glm/glm.hpp>
+#include <iostream>
+#include <vector>
 
-#include "render_utils.hpp"
-#include "types.hpp"
 #include "object.hpp"
 #include "physics.cc"
+#include "render_utils.hpp"
+#include "types.hpp"
 
 ////////////// FORCE GENERATOR
 
 class ForceGenerator
 {
 public:
-    virtual void updateForce(Object &object, float timestep) = 0;
+  virtual void updateForce(Object &object, float timestep) = 0;
 
-    bool operator==(ForceGenerator &fg)
-    {
-        return true;
-    };
+  bool operator==(ForceGenerator &fg) { return true; };
 
-    bool operator==(const ForceGenerator &fg)
-    {
-        return true;
-    };
+  bool operator==(const ForceGenerator &fg) { return true; };
 };
 
 class Gravity : public ForceGenerator
 {
 protected:
-    float gravity; // gravity constant
+  float gravity; // gravity constant
 
 public:
-    // TODO: brush up on move semantics
-    Gravity(float gravity)
-    {
-        this->gravity = gravity;
-    }
+  // TODO: brush up on move semantics
+  Gravity(float gravity) { this->gravity = gravity; }
 
-    void updateForce(Object &obj, float timestep)
-    {
-        obj.force += glm::vec3(0.0f, obj.mass * gravity, 0.0f);
-    }
+  void updateForce(Object &obj, float timestep)
+  {
+    obj.force += glm::vec3(0.0f, obj.mass * gravity, 0.0f);
+  }
 
-    // bool operator==(ForceGenerator &fg)
-    // {
-    //     return true; // this part is not important
-    // };
+  // bool operator==(ForceGenerator &fg)
+  // {
+  //     return true; // this part is not important
+  // };
 };
 
 // TODO: ADD DAMPING FORCE (or just update vel )
@@ -58,117 +49,137 @@ public:
 class PlaneCollision : public ForceGenerator
 {
 protected:
-    float floorY;
+  float floorY;
 
 public:
-    // TODO: brush up on move semantics
-    PlaneCollision(float floorY)
+  // TODO: brush up on move semantics
+  PlaneCollision(float floorY) { this->floorY = floorY; }
+
+  void updateForce(Object &obj, float timestep)
+  {
+
+    // Floor plane collision
+    switch (obj.type)
     {
-        this->floorY = floorY;
+    case ObjectType::Sphere:
+    {
+      Sphere &sph = static_cast<Sphere &>(obj);
+      if (sph.position.y - sph.radius < floorY)
+      {
+        sph.position.y = floorY + sph.radius;
+        sph.velocity.y = -sph.velocity.y * 0.8f; // Simple bounce with damping
+      }
     }
-
-    void updateForce(Object &obj, float timestep)
+    break;
+    case ObjectType::Cube:
     {
 
-        // Floor plane collision
-        switch (obj.type)
-        {
-        case ObjectType::Sphere:
-            if (obj.position.y - obj.size < floorY)
-            {
-                obj.position.y = floorY + obj.size;
-                obj.velocity.y = -obj.velocity.y * 0.8f; // Simple bounce with damping
-            }
-            break;
-        case ObjectType::Cube:
-            if (obj.position.y - obj.size / 2.0f < floorY)
-            {
-                obj.position.y = floorY + obj.size / 2.0f;
-                obj.velocity.y = -obj.velocity.y * 0.8f;
-            }
-            break;
-        case ObjectType::Cylinder:
-            if (obj.position.y - obj.height / 2.0f < floorY)
-            {
-                obj.position.y = floorY + obj.height / 2.0f;
-                obj.velocity.y = -obj.velocity.y * 0.8f;
-            }
-            break;
-        case ObjectType::Cone:
-            if (obj.position.y - obj.height / 2.0f < floorY)
-            {
-                obj.position.y = floorY + obj.height / 2.0f;
-                obj.velocity.y = -obj.velocity.y * 0.8f;
-            }
-            break;
-        case ObjectType::Torus:
-            if (obj.position.y - obj.size < floorY)
-            {
-                obj.position.y = floorY + obj.size;
-                obj.velocity.y = -obj.velocity.y * 0.8f;
-            }
-            break;
-        case ObjectType::Cuboid:
-            if (obj.position.y - obj.height / 2.0f < floorY)
-            {
-                obj.position.y = floorY + obj.height / 2.0f;
-                obj.velocity.y = -obj.velocity.y * 0.8f;
-            }
-            break;
-        }
+      Cube &cub = static_cast<Cube &>(obj);
+      if (cub.position.y - cub.size / 2.0f < floorY)
+      {
+        cub.position.y = floorY + cub.size / 2.0f;
+        cub.velocity.y = -cub.velocity.y * 0.8f;
+      }
     }
-
-    bool operator==(ForceGenerator &fg)
+    break;
+    case ObjectType::Cylinder:
     {
-        return true; // this part is not important
-    };
+
+      Cylinder &cyl = static_cast<Cylinder &>(obj);
+      if (cyl.position.y - cyl.height / 2.0f < floorY)
+      {
+        cyl.position.y = floorY + cyl.height / 2.0f;
+        cyl.velocity.y = -cyl.velocity.y * 0.8f;
+      }
+    }
+    break;
+    case ObjectType::Cone:
+    {
+
+      Cone &con = static_cast<Cone &>(obj);
+      if (con.position.y - con.height / 2.0f < floorY)
+      {
+        con.position.y = floorY + con.height / 2.0f;
+        con.velocity.y = -con.velocity.y * 0.8f;
+      }
+    }
+    break;
+    case ObjectType::Torus:
+    {
+
+      Torus &tor = static_cast<Torus &>(obj);
+      if (tor.position.y - tor.radius < floorY)
+      {
+        tor.position.y = floorY + tor.radius;
+        tor.velocity.y = -tor.velocity.y * 0.8f;
+      }
+    }
+    break;
+    case ObjectType::Cuboid:
+    {
+
+      Cuboid &cuboid = static_cast<Cuboid &>(obj);
+      if (cuboid.position.y - cuboid.height / 2.0f < floorY)
+      {
+        cuboid.position.y = floorY + cuboid.height / 2.0f;
+        obj.velocity.y = -obj.velocity.y * 0.8f;
+      }
+    }
+    break;
+    }
+  }
+
+  bool operator==(ForceGenerator &fg)
+  {
+    return true; // this part is not important
+  };
 };
 
 class Propulsion : public ForceGenerator
 {
 protected:
-    float gravity;
-    float k;
+  float gravity;
+  float k;
 
 public:
-    Propulsion(float k, float gravity)
-    {
-        this->k = k;
-        this->gravity = gravity;
-    }
-    void updateForce(Object &obj, float timestep)
-    {
+  Propulsion(float k, float gravity)
+  {
+    this->k = k;
+    this->gravity = gravity;
+  }
+  void updateForce(Object &obj, float timestep)
+  {
 
-        //  what would the force be?
-        // neglect gravity
-        // we dont have shifting mass so we cant actually do it
-        // we can try to make it inclined though
-        if (obj.velocity.y < 0)
-        {
-            obj.force += glm::vec3(0.0f, 0.0f, obj.mass * 1.1f);
-        }
-        else
-        {
-            obj.force += glm::vec3(obj.mass * 1.1f, 0.0f, 0.0f);
-        }
-        obj.force += glm::vec3(0.0f, obj.mass * -gravity * k, 0.0f);
+    //  what would the force be?
+    // neglect gravity
+    // we dont have shifting mass so we cant actually do it
+    // we can try to make it inclined though
+    if (obj.velocity.y < 0)
+    {
+      obj.force += glm::vec3(0.0f, 0.0f, obj.mass * 1.1f);
     }
+    else
+    {
+      obj.force += glm::vec3(obj.mass * 1.1f, 0.0f, 0.0f);
+    }
+    obj.force += glm::vec3(0.0f, obj.mass * -gravity * k, 0.0f);
+  }
 };
 
 struct ForceRegistration
 {
-    Object &object;
-    ForceGenerator &fg;
+  Object &object;
+  ForceGenerator &fg;
 
-    bool operator==(ForceRegistration &fr)
-    {
-        return object == fr.object && fg == fr.fg;
-    }
+  bool operator==(ForceRegistration &fr)
+  {
+    return object == fr.object && fg == fr.fg;
+  }
 
-    bool operator==(const ForceRegistration &fr)
-    {
-        return object == fr.object && fg == fr.fg;
-    }
+  bool operator==(const ForceRegistration &fr)
+  {
+    return object == fr.object && fg == fr.fg;
+  }
 };
 
 typedef std::vector<ForceRegistration> Registry;
@@ -176,44 +187,41 @@ typedef std::vector<ForceRegistration> Registry;
 class ForceRegistry
 {
 protected:
-    Registry registrations;
+  Registry registrations;
 
 public:
-    ForceRegistry()
-    {
-        // nothing
-    }
-    void add(Object &object, ForceGenerator &fg)
-    {
-        registrations.push_back(ForceRegistration{object, fg});
-    }
+  ForceRegistry()
+  {
+    // nothing
+  }
+  void add(Object &object, ForceGenerator &fg)
+  {
+    registrations.push_back(ForceRegistration{object, fg});
+  }
 
-    // TODO : fix static error asser problem on .erase(it)
-    //  void remove(Object &object, ForceGenerator &fg)
-    //  {
-    //      ForceRegistration f = ForceRegistration{object, fg};
-    //      auto it = std::find(registrations.begin(), registrations.end(), f);
-    //      if (it != registrations.end())
-    //          registrations.erase(it);
-    //      else
-    //          std::cerr << "Could not remove object \n";
-    //  }
+  // TODO : fix static error asser problem on .erase(it)
+  //  void remove(Object &object, ForceGenerator &fg)
+  //  {
+  //      ForceRegistration f = ForceRegistration{object, fg};
+  //      auto it = std::find(registrations.begin(), registrations.end(), f);
+  //      if (it != registrations.end())
+  //          registrations.erase(it);
+  //      else
+  //          std::cerr << "Could not remove object \n";
+  //  }
 
-    void clear(Object &object, ForceGenerator &fg)
-    {
-        registrations.clear();
-    }
+  void clear(Object &object, ForceGenerator &fg) { registrations.clear(); }
 
-    void updateForces(float duration)
-    {
+  void updateForces(float duration)
+  {
 
-        // reset forces
-        for (auto &k : registrations)
-            k.object.force = glm::vec3(0.0f);
+    // reset forces
+    for (auto &k : registrations)
+      k.object.force = glm::vec3(0.0f);
 
-        for (auto &k : registrations)
-            k.fg.updateForce(k.object, duration);
-    }
+    for (auto &k : registrations)
+      k.fg.updateForce(k.object, duration);
+  }
 };
 
 ////////////
@@ -228,131 +236,151 @@ auto prop = Propulsion(1.03f, -9.81f);
 
 void render()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    // gluLookAt(0, 5, 10, 0, 0, 0, 0, 1, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  // gluLookAt(0, 5, 10, 0, 0, 0, 0, 1, 0);
 
-    gluLookAt(0.0, 0.0, 6.0, 0.0, 0.0, 0.0, 0.0, 3.0, 0.0); // Set up the camera position
+  gluLookAt(0.0, 0.0, 6.0, 0.0, 0.0, 0.0, 0.0, 3.0,
+            0.0); // Set up the camera position
 
-    // CAMERA UPDATE
-    glRotatef(angleX, 1.0, 0.0, 0.0);
-    glRotatef(angleY, 0.0, 1.0, 0.0);
+  // CAMERA UPDATE
+  glRotatef(angleX, 1.0, 0.0, 0.0);
+  glRotatef(angleY, 0.0, 1.0, 0.0);
 
-    // Set camera position
-    glTranslatef(-cameraX, -cameraY, -cameraZ);
+  // Set camera position
+  glTranslatef(-cameraX, -cameraY, -cameraZ);
 
-    renderFloorPlane(100.0f);
+  renderFloorPlane(100.0f);
 
-    // TODO: work on better obj rendering (when the type gets updated)
-    glColor3f(1.0f, 0.0f, 0.0f);
-    for (const auto &obj : objects)
+  // TODO: work on better obj rendering (when the type gets updated)
+  glColor3f(1.0f, 0.0f, 0.0f);
+  for (Object &obj : objects)
+  {
+
+    glPushMatrix();
+    glTranslatef(obj.position.x, obj.position.y, obj.position.z);
+    switch (obj.type)
+    {
+    case ObjectType::Sphere:
     {
 
-        glPushMatrix();
-        glTranslatef(obj.position.x, obj.position.y, obj.position.z);
+      Sphere &sph = static_cast<Sphere &>(obj);
+      glutSolidSphere(sph.radius, 20, 20);
+    }
+    break;
+    case ObjectType::Cube:
+    {
+      Cube &cub = static_cast<Cube &>(obj);
+      glutSolidCube(cub.size * 2.0f);
+    }
+    break;
+    case ObjectType::Cylinder:
+    {
+      Cylinder &cyl = static_cast<Cylinder &>(obj);
+      glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+      glutSolidCylinder(cyl.radius, cyl.height, 20, 20);
+    }
+    break;
+    case ObjectType::Cone:
+    {
+      Cone &con = static_cast<Cone &>(obj);
+      glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+      glutSolidCone(con.radius, con.height, 20, 20);
+    }
+    break;
+    case ObjectType::Torus:
+    {
+      Torus &tor = static_cast<Torus &>(obj);
+      glutSolidTorus(tor.radius * 0.5f, tor.thickness, 20, 20);
+    }
+    break;
 
-        switch (obj.type)
-        {
-        case ObjectType::Sphere:
-            glutSolidSphere(obj.size, 20, 20);
-            break;
-        case ObjectType::Cube:
-            glutSolidCube(obj.size * 2.0f);
-            break;
-        case ObjectType::Cylinder:
-            glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-            glutSolidCylinder(obj.size, obj.height, 20, 20);
-            break;
-        case ObjectType::Cone:
-            glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-            glutSolidCone(obj.size, obj.height, 20, 20);
-            break;
-        case ObjectType::Torus:
-            glutSolidTorus(obj.size * 0.5f, obj.size, 20, 20);
-            break;
+    case ObjectType::Cuboid:
+    {
 
-        case ObjectType::Cuboid:
-            glScalef(obj.size, obj.height, obj.width);
-            glutSolidCube(1.0f);
-            break;
-        }
-
-        glPopMatrix();
+      Cuboid &cuboid = static_cast<Cuboid &>(obj);
+      glScalef(cuboid.length, cuboid.height, cuboid.width);
+      glutSolidCube(1.0f);
+    }
+    break;
     }
 
-    updateFPS();
-    drawFPS();
+    glPopMatrix();
+  }
 
-    glutSwapBuffers();
+  updateFPS();
+  drawFPS();
+
+  glutSwapBuffers();
 }
 
 void loop(int value)
 {
 
-    // updatePhysics(objects, 0.0f); // floorPlane is at y0 for now
-    fr.updateForces(timeStep);
+  // updatePhysics(objects, 0.0f); // floorPlane is at y0 for now
+  fr.updateForces(timeStep);
 
-    for (auto &obj : objects)
-    {
-        std::cout << obj.force.x << " " << obj.force.y << " " << obj.force.z << std::endl;
-        obj.updatePhysics(timeStep);
-    }
-    glutPostRedisplay();
-    glutTimerFunc(16, loop, 0);
+  for (auto &obj : objects)
+  {
+    // std::cout << obj.force.x << " " << obj.force.y << " " << obj.force.z
+    //           << std::endl;
+    obj.updatePhysics(timeStep);
+  }
+  glutPostRedisplay();
+  glutTimerFunc(16, loop, 0);
 }
 
 void init()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glEnable(GL_DEPTH_TEST);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glEnable(GL_DEPTH_TEST);
+  // Create some sample objects (very temporary)
+  Sphere sphere({.position = glm::vec3(0.0f, 5.0f, 0.0f)}, 1.5f);
+  Cube cube({.position = glm::vec3(10.0f, 0.0f, 0.0f)}, 2.0f);
+  Cylinder cylinder({.position = glm::vec3(20.0f, 3.0f, 0.0f)}, 1.5f, 2.0f);
+  Cuboid cuboid({.position = glm::vec3(-22.0f, 0.0f, 0.0f)}, 2.0, 4.0f, 6.0f);
 
-    // Create some sample objects (very temporary)
-    Object sphere(glm::vec3(0.0f, 5.0f, 0.0f), 1.0f, 1.5f, ObjectType::Sphere);
-    Object cube(glm::vec3(10.0f, 0.0f, 0.0f), 1.0f, 2.0f, ObjectType::Cube);
-    Object cylinder(glm::vec3(20.0f, 3.0f, 0.0f), 1.5f, 2.0f, 2.0f, ObjectType::Cylinder);
-    Object cuboid(glm::vec3(-22.0f, 0.0f, 0.0f), 1.0f, 2.0f, 4.0f, 6.0f, ObjectType::Cube);
+  // sphere.setVelocity(glm::vec3(4.0, 0.0, 3.0));
+  // cube.setVelocity(glm::vec3(10.0, 0.0, 0.0));
+  // cuboid.setVelocity(glm::vec3(0.0f, 1.0f, 0.0f));
 
-    // sphere.setVelocity(glm::vec3(4.0, 0.0, 3.0));
-    // cube.setVelocity(glm::vec3(10.0, 0.0, 0.0));
-    // cuboid.setVelocity(glm::vec3(0.0f, 1.0f, 0.0f));
+  // cylinder.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
+  // cube.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
+  // cuboid.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
 
-    // cylinder.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
-    // cube.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
-    // cuboid.setAccel(glm::vec3(0.0f, 0.0f, 0.0f));
+  objects.push_back(sphere);
+  objects.push_back(cube);
+  // objects.push_back(cuboid);
+  // objects.push_back(cylinder);
 
-    objects.push_back(sphere);
-    objects.push_back(cube);
-    objects.push_back(cuboid);
-    objects.push_back(cylinder);
-
-    for (auto &obj : objects)
-    {
-        fr.add(obj, grav);
-        fr.add(obj, fplane_force);
-    }
-    fr.add(objects.at(0), prop);
+  for (auto &obj : objects)
+  {
+    fr.add(obj, grav);
+    fr.add(obj, fplane_force);
+  }
+  fr.add(objects.at(0), prop);
 }
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-    glutInitWindowSize(1000, 800);
-    glutCreateWindow("RGB");
+  glutInitWindowSize(1000, 800);
+  glutCreateWindow("RGB");
 
-    // glutSetCursor(GLUT_CURSOR_INFO); // glove
+  // glutSetCursor(GLUT_CURSOR_INFO); // glove
 
-    glutDisplayFunc(render);
-    glutReshapeFunc(reshape);
-    glutMotionFunc(motion);
-    glutKeyboardFunc(keyboard);
-    glutMouseFunc(mouseWheel);
+  glutDisplayFunc(render);
+  glutReshapeFunc(reshape);
+  glutMotionFunc(motion);
+  glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouseWheel);
 
-    glutTimerFunc(0, loop, 0);
+  glutTimerFunc(0, loop, 0);
 
-    init();
-    glutMainLoop();
-    return 0;
+  init();
+  glutMainLoop();
+  return 0;
 }
--- src/object.hpp
+++ src/object.hpp
@@ -5,110 +5,193 @@
 
 enum class ObjectType
 {
-    Sphere,
-    Cube,
-    Cylinder,
-    Cone,
-    Torus,
-    Cuboid
+  Sphere,
+  Cube,
+  Cylinder,
+  Cone,
+  Torus,
+  Cuboid
 };
 
 // very temp, change at will!
-// TODO: we need to store diff attributes for diff object types, I'll leave that to you
+// TODO: we need to store diff attributes for diff object types, I'll leave that
+// to you
 
-struct Object
+struct ObjectArgs
 {
-    glm::vec3 position;
-    glm::vec3 velocity;
-    glm::vec3 acceleration;
-    glm::vec3 force;
-    float mass;
-    float size; // radius or length
-    float height;
-    float width;
-    ObjectType type;
-
-    Object()
-    {
-        position = glm::vec3(0.0f);
-        velocity = glm::vec3(0.0f);
-        acceleration = glm::vec3(0.0f);
-        force = glm::vec3(0.0f);
-        mass = 1.0f;
-        size = 1.0f;
-        type = ObjectType::Sphere;
-    }
-
-    Object(const glm::vec3 &pos, float m, float l, ObjectType t)
-    {
-        position = pos;
-        velocity = glm::vec3(0.0f);
-        acceleration = glm::vec3(0.0f);
-        force = glm::vec3(0.0f);
-        mass = m;
-        size = l;
-        type = t;
-    }
-
-    Object(const glm::vec3 &pos, float m, float l, float h, ObjectType t)
-    {
-        position = pos;
-        velocity = glm::vec3(0.0f);
-        acceleration = glm::vec3(0.0f);
-        force = glm::vec3(0.0f);
-        mass = m;
-        size = l;
-        height = h;
-        type = t;
-    }
-
-    Object(const glm::vec3 &pos, float m, float l, float h, float w, ObjectType t)
-    {
-        position = pos;
-        velocity = glm::vec3(0.0f);
-        acceleration = glm::vec3(0.0f);
-        force = glm::vec3(0.0f);
-        mass = m;
-        size = l;
-        height = h;
-        width = w;
-        type = t;
-    }
-
-    void setVelocity(const glm::vec3 &vel)
-    {
-        this->velocity = vel;
-    }
-
-    void setAccel(const glm::vec3 &accel)
-    {
-        this->acceleration = accel;
-    }
-
-    void setForce(const glm::vec3 &f)
-    {
-        this->force = f;
-    }
-
-    void applyForce(const glm::vec3 &f)
-    {
-        this->force += f;
-    }
-
-    void updatePhysics(float timeStep)
-    {
-        acceleration = force / mass;
-        velocity += acceleration * 0.9999f * timeStep;
-        position += velocity * timeStep;
-    }
-
-    bool operator==(Object &obj)
-    {
-        return mass == obj.mass && size == obj.size && position == obj.position && velocity == obj.velocity && acceleration == obj.acceleration;
-    }
+  glm::vec3 position = glm::vec3(0.0f);
+  glm::vec3 velocity = glm::vec3(0.0f);
+  glm::vec3 acceleration = glm::vec3(0.0f);
+  glm::vec3 force = glm::vec3(0.0f);
+  float mass = 1.0f;
 };
 
-// void load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec3> &normals, vector<GLushort> &elements)
+class Object
+{
+public:
+  glm::vec3 position;
+  glm::vec3 velocity;
+  glm::vec3 acceleration;
+  glm::vec3 force;
+  float mass;
+  ObjectType type;
+
+  Object(ObjectArgs args, ObjectType type)
+  {
+    this->position = args.position;
+    this->velocity = args.velocity;
+    this->acceleration = args.acceleration;
+    this->force = args.force;
+    this->mass = args.mass;
+    this->type = type;
+  }
+
+  void setVelocity(const glm::vec3 &vel) { this->velocity = vel; }
+
+  void setAccel(const glm::vec3 &accel) { this->acceleration = accel; }
+
+  void setForce(const glm::vec3 &f) { this->force = f; }
+
+  void applyForce(const glm::vec3 &f) { this->force += f; }
+
+  void updatePhysics(float timeStep)
+  {
+    acceleration = force / mass;
+    velocity += acceleration * 0.9999f * timeStep;
+    position += velocity * timeStep;
+  }
+  bool partialEq(Object &obj)
+  {
+    return mass == obj.mass && position == obj.position &&
+           velocity == obj.velocity && acceleration == obj.acceleration;
+  }
+
+  virtual bool operator==(Object &object)
+  {
+    return this->partialEq(object);
+  };
+};
+
+class Sphere : public Object
+{
+public:
+  float radius;
+  Sphere(ObjectArgs object, float radius)
+      : Object(object, ObjectType::Sphere), radius(radius){};
+  bool operator==(Object &obj)
+      override
+  {
+    Sphere &sph = static_cast<Sphere &>(obj);
+    if (sph.partialEq(sph) && sph.radius == radius)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+class Cube : public Object
+{
+public:
+  float size;
+  Cube(ObjectArgs object, float size)
+      : Object(object, ObjectType::Cube), size(size){};
+  bool operator==(Object &obj)
+      override
+  {
+    Cube &cub = static_cast<Cube &>(obj);
+    if (cub.partialEq(cub) && cub.size == size)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+class Cylinder : public Object
+{
+public:
+  float radius;
+  float height;
+  Cylinder(ObjectArgs object, float radius, float height)
+      : Object(object, ObjectType::Cylinder), radius(radius), height(height){};
+  bool operator==(Object &obj)
+      override
+  {
+    Cylinder &cyl = static_cast<Cylinder &>(obj);
+    if (cyl.partialEq(cyl) && cyl.radius == radius && cyl.height == height)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+class Cone : public Object
+{
+public:
+  float radius;
+  float height;
+  Cone(ObjectArgs object, float radius, float height)
+      : Object(object, ObjectType::Cone), radius(radius), height(height){};
+
+  bool operator==(Object &obj)
+      override
+  {
+    Cone &con = static_cast<Cone &>(obj);
+    if (con.partialEq(con) && con.radius == radius && con.height == height)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+class Torus : public Object
+{
+public:
+  float radius;
+  float thickness;
+  Torus(ObjectArgs object, float radius, float thickness)
+      : Object(object, ObjectType::Torus), radius(radius),
+        thickness(thickness){};
+
+  bool operator==(Object &obj)
+      override
+  {
+    Torus &tor = static_cast<Torus &>(obj);
+    if (tor.partialEq(tor) && tor.radius == radius && tor.thickness == thickness)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+class Cuboid : public Object
+{
+public:
+  float width;
+  float height;
+  float length;
+  Cuboid(ObjectArgs object, float width, float height, float length)
+      : Object(object, ObjectType::Cuboid), width(width), height(height),
+        length(length){};
+  bool operator==(Object &obj)
+      override
+  {
+    Cuboid &cuboid = static_cast<Cuboid &>(obj);
+    if (cuboid.partialEq(cuboid) && cuboid.width == width &&
+        cuboid.height == height && cuboid.length == length)
+    {
+      return true;
+    }
+    return false;
+  }
+};
+
+// void load_obj(const char* filename, vector<glm::vec4> &vertices,
+// vector<glm::vec3> &normals, vector<GLushort> &elements)
 // {
 //     ifstream in(filename, ios::in);
 //     if (!in)
@@ -131,7 +214,8 @@ struct Object
 //             GLushort a,b,c;
 //             s >> a; s >> b; s >> c;
 //             a--; b--; c--;
-//            elements.push_back(a); elements.push_back(b); elements.push_back(c);
+//            elements.push_back(a); elements.push_back(b);
+//            elements.push_back(c);
 //         }
 //         /* anything else is ignored */
 //     }
